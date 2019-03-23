5a8a597729685273766bb77b73f5c51a167b69c0
Glitch (ecommerce-reactjs) <none>
2019-03-23 10:34:50 +0000

    🚜⏳ Updated with Glitch

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import {ProductProvider} from './Context'
+
+ReactDOM.render(
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>
+    
+  ,
+  document.getElementById('root')
+);
